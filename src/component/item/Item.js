7b0807651fedf55fa1681dba900e37b0159ce253
Nattawod Satee <nattawodsatee@Nattawods-MacBook-Pro.local>
2019-07-17 11:02:56 -0700

    Implement delete item UI

--- src/component/item/Item.js
+++ src/component/item/Item.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { checkItem } from '../../store/actions/listAction'
 import { connect } from 'react-redux';
-import {FiX} from 'react-icons/fi'
+import { FiX } from 'react-icons/fi'
 
 class Item extends Component {
     render() {
         return (
             <li className={`item ${this.props.isDone ? "done" : "notdone"}`}>
-                <a href="/" 
+                <div className="delete-icon">
+                    <button className="icon">
+                        <FiX />
+                        <span>delete</span>
+                    </button>
+                </div>
+                <a href="/"
                     onClick={(e) => {
                         e.preventDefault();
                         this.props.checkToggle(this.props.itemId, this.props.isDone);
-                        }}
-                    >
+                    }}
+                >
                     <span className="checkbox">
                         {this.props.isDone ? <span className="check"></span> : null}
                     </span>
